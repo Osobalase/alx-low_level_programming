808c10f4bc1f8fdd93f5daf83dfb61b8b5763c2d
Osobalase <oxblade4real@gmail.com>
2022-10-21 02:21:00 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main. h"
 
 /**
- * _isdigit - write a function that check for a digit (0 through 9).
+ * _isdigit - check if a number is 0 - 9
  * @c: char to check
  * Return: 0 or 1
  */
