0b2f8a7dd80258a3a13e29cf5e0d0bc81ae009d8
Osobalase <oxblade4real@gmail.com>
2022-10-21 17:15:03 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
- * owned by Osobalase
+ * print_most_numbers - print num except 2 and 4
+ *
  * Return: void
  */
 
@@ -12,8 +12,8 @@ void print_most_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (!(c == '2' || c == '4'))
+		_putchar(c);
 	}
 	_putchar('\n');
 }
