d305b10d2a49bd3be92dd6c77ff837829f6bb295
Osobalase <oxblade4real@gmail.com>
2022-10-27 15:06:54 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcat -> this is a function strcat
- * @dest: first parameter
- * @src: second parameter
+ * @dest: first param
+ * @src: second param
  * Return: a string
  */
 char *_strcat(char *dest, char *src)
