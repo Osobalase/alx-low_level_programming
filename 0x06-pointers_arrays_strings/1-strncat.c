e09a3eb10895d16febabe6a5f787352d4e00c13e
Osobalase <oxblade4real@gmail.com>
2022-10-27 16:39:20 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,17 +1,29 @@
-#include "makn.h"
-#include <string.h>
+#include "main.h"
 
 /**
- * @dest: first param
- * @src: secod param
- * @n: 3rd param
- * Retrn: string
+ * _strncat - joins two strings with n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char dest[100] = "HELLO";
-	char *src = "wprld';
-	strncat(dest, src, 3);
-	printf("%s", dest);
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
