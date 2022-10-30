61e949fc5709d208531f1cd2ddf52814160f2866
Osobalase <oxblade4real@gmail.com>
2022-10-30 00:15:12 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all strings from 0 to 9
+ * Return: ALways 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+
+	printf("\n");
+
+	return (0);
+}
