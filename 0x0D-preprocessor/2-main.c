2a028fb1ffdaa2fea03dc8c4d56eb3b53e7d1598
Osobalase <oxblade4real@gmail.com>
2022-11-15 20:57:29 -0800

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /** 
- * program - prints the name of the file compiled from
+ * main - prints the name of the file compiled from
  * Return: Always 0
  */
 
-int program(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
