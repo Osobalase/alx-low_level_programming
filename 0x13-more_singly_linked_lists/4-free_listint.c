fa7408e1cc738c3b9451b83a9d04bd7f95bb7f29
Osobalase <oxblade4real@gmail.com>
2022-11-29 19:41:08 -0800

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,13 +3,12 @@
 /**
  * free_listint - frees a linked list
  * @head: head of list
- * Return: always 0
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while ((temp = heas) != NULL)
+	while ((temp = head) != NULL)
 	{
 		head = head->next;
 		free(temp);
