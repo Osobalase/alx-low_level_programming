c2a2163e72a270b259da8882a7a4b6ed7ce35a3e
Osobalase <oxblade4real@gmail.com>
2022-11-29 19:59:29 -0800

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * pop_listint - deletes the head node
+ * of a linked list
+ * @head: head of a list
+ * Return: head node's data. if empty - 0.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
+}
